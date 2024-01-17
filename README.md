a0f90534b8cbd23a0f7a467659a7f3ce42ac643e
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2024-01-17 16:49:22 +0530

    Add files via upload
    done

---
+++ README.md
@@ -0,0 +1,31 @@
+// instructions 
+
+To run the Project , run these two commands in seprate terminals
+
+```
+npm run watch
+```
+
+```
+npm run dev
+```
+
+
+### Dummy Data 
+
+user1
+
+{
+    "username": "rahul",
+    "email": "firedr4gon7@gmail.com",
+    "password": "rahulPass@123"
+}
+
+
+user2
+
+{
+    "username": "arpit_khanulia",
+    "email": "arpitkhanulia7@gmail.com",
+    "password": "arpitPass@123"
+}
---
+++ src/model/users.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    wallet: {
+        type: Number,
+        required: true
+    },
+    transition:{
+        type: Array,
+        required:true
+    }
+
+});
+
+
+
+// model
+const User = mongoose.model('User', userSchema);
+
+
+export {User};
+
+
